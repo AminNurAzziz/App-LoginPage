d927f1b8e01e232584dd13b301155e3aa0c4ceab
Amin Nur Azziz <2131710034@student.polinema.ac.id>
2023-11-01 23:02:10 +0700

    add API login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,204 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// class LoginPage extends StatefulWidget {
+//   const LoginPage({super.key});
+
+//   @override
+//   State<LoginPage> createState() => _LoginPageState();
+// }
+
+// class _LoginPageState extends State<LoginPage> {
+//   bool rememberMe = false;
+//   bool isPasswordVisible = false;
+//   TextEditingController passwordController = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         height: double.infinity,
+//         width: double.infinity,
+//         decoration: BoxDecoration(
+//           gradient: LinearGradient(
+//             colors: [
+//               Theme.of(context).colorScheme.primary,
+//               Color.fromARGB(255, 255, 255, 255)
+//             ],
+//             begin: Alignment.topCenter,
+//             end: Alignment.bottomCenter,
+//           ),
+//         ),
+//         child: SafeArea(
+//           child: SingleChildScrollView(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 Container(
+//                   height: 340,
+//                   width: double.infinity,
+//                   child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [
+//                       Text(
+//                         'FINANCE \n APP',
+//                         style: GoogleFonts.lato(
+//                           fontSize: 32,
+//                           fontWeight: FontWeight.bold,
+//                           color: Colors.white,
+//                         ),
+//                         textAlign: TextAlign.center,
+//                       ),
+//                       SizedBox(height: 20),
+//                       Image.asset(
+//                         'assets/images/Banner.png',
+//                         height: 180,
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//                 // Container Login
+//                 Container(
+//                   padding: EdgeInsets.all(20.0),
+//                   width: double.infinity,
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     borderRadius: BorderRadius.vertical(
+//                       top: Radius.circular(30.0),
+//                     ),
+//                   ),
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Text(
+//                         'Welcome',
+//                         style: TextStyle(
+//                           fontSize: 30,
+//                           fontWeight: FontWeight.bold,
+//                           color: Theme.of(context).colorScheme.primary,
+//                         ),
+//                       ),
+//                       SizedBox.fromSize(size: Size(0, 5)),
+//                       Text(
+//                         'Please login with your account',
+//                         style: TextStyle(
+//                           fontSize: 14,
+//                           color: Colors.grey,
+//                         ),
+//                       ),
+//                       SizedBox.fromSize(size: Size(0, 20)),
+//                       Text(
+//                         'Username',
+//                         style: TextStyle(
+//                           fontSize: 18,
+//                           color: const Color.fromARGB(255, 71, 73, 77),
+//                         ),
+//                       ),
+//                       TextField(
+//                         decoration: InputDecoration(
+//                           suffixIcon: Icon(Icons.check), // Checkmark icon
+//                           suffixIconConstraints: BoxConstraints(
+//                             maxHeight: 32,
+//                             maxWidth: 32,
+//                           ),
+//                         ),
+//                       ),
+//                       SizedBox(height: 20),
+//                       Text(
+//                         'Password',
+//                         style: TextStyle(
+//                           fontSize: 18,
+//                           color: const Color.fromARGB(255, 71, 73, 77),
+//                         ),
+//                       ),
+//                       TextField(
+//                         controller: passwordController,
+//                         decoration: InputDecoration(
+//                           suffixIcon: IconButton(
+//                             icon: isPasswordVisible
+//                                 ? Icon(Icons.visibility)
+//                                 : Icon(Icons.visibility_off),
+//                             onPressed: () {
+//                               setState(() {
+//                                 isPasswordVisible = !isPasswordVisible;
+//                               });
+//                             },
+//                           ),
+//                           suffixIconConstraints: BoxConstraints(
+//                             maxHeight: 32,
+//                             maxWidth: 32,
+//                           ),
+//                         ),
+//                         obscureText:
+//                             !isPasswordVisible, // Menyembunyikan atau menampilkan password
+//                       ),
+//                       SizedBox(height: 20),
+//                       Row(
+//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                         children: [
+//                           Row(
+//                             children: [
+//                               Checkbox(
+//                                 value: rememberMe,
+//                                 onChanged: (value) {
+//                                   setState(() {
+//                                     rememberMe = value!;
+//                                   });
+//                                 },
+//                               ),
+//                               Text('Remember Me'),
+//                             ],
+//                           ),
+//                           Spacer(), // Spacer pushes elements to the right
+//                           Text('Forget Password?'),
+//                         ],
+//                       ),
+//                       SizedBox(height: 20),
+//                       Container(
+//                         width: double.infinity,
+//                         height: 50,
+//                         child: ElevatedButton(
+//                           onPressed: () {},
+//                           child: Text('Login'),
+//                           style: ElevatedButton.styleFrom(
+//                             primary: Theme.of(context).colorScheme.primary,
+//                             shape: RoundedRectangleBorder(
+//                               borderRadius: BorderRadius.circular(10),
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                       SizedBox(height: 20),
+//                       Row(
+//                         mainAxisAlignment: MainAxisAlignment.center,
+//                         children: [
+//                           Text("Don't have an account?"),
+//                           TextButton(
+//                             onPressed: () {},
+//                             child: Text('Sign Up'),
+//                           ),
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                 )
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// // nGrog
+// //
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'after_login.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,7 +210,65 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   bool rememberMe = false;
   bool isPasswordVisible = false;
+  bool isLoading = false;
+
+  TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  Future<void> login() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    final response = await http.post(
+      Uri.parse("http://192.168.178.135:4000/"),
+      body: {
+        'username': usernameController.text,
+        'password': passwordController.text,
+      },
+    );
+
+    setState(() {
+      isLoading = false;
+    });
+
+    if (response.statusCode == 200 && json.decode(response.body)['success']) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Success'),
+          content: Text('Login successful!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => AfterLogin()),
+                );
+              },
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('Login failed! Please try again.' +
+              json.decode(response.body)['message']),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -95,6 +352,7 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                       ),
                       TextField(
+                        controller: usernameController,
                         decoration: InputDecoration(
                           suffixIcon: Icon(Icons.check), // Checkmark icon
                           suffixIconConstraints: BoxConstraints(
@@ -158,7 +416,7 @@ class _LoginPageState extends State<LoginPage> {
                         width: double.infinity,
                         height: 50,
                         child: ElevatedButton(
-                          onPressed: () {},
+                          onPressed: login, // Use the login method
                           child: Text('Login'),
                           style: ElevatedButton.styleFrom(
                             primary: Theme.of(context).colorScheme.primary,
@@ -192,4 +450,4 @@ class _LoginPageState extends State<LoginPage> {
 }
 
 // nGrog
-// 
+//
